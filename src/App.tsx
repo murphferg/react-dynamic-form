e5e018af405abf929d5d91ad9e331da7fd8e11f1
murphferg <kevin@fergs.com>
2024-02-25 17:34:25 -0500

    Working prototype

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import React, { useEffect, useState }  from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { parseFormJson } from './parseJson';
-import { FormElement } from './jsonTypes';
+import { FormElement, InputCheckbox, InputSelect, InputText } from './jsonTypes';
+import InputTextComponent from './InputTextComponent';
+import InputCheckboxComponent from './InputCheckboxComponent';
+import InputSelectComponent from './InputSelectComponent';
 
 function App() {
 
   const [formElements, setFormElements] = useState<FormElement[] | null>(null);
 
   useEffect(() => {
-    fetch('./form.json')
+    fetch('./data/form.json', { headers: { 'Accept': 'application/json' } })
       .then(response => response.json())
       .then(data => {
         const parsedData = parseFormJson(JSON.stringify(data));
@@ -29,14 +32,16 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {formElements?.map((element, index) => {
+          if ('text' in element) {
+            return <InputTextComponent key={index} inputText={element as InputText} />;
+          } else if ('checkbox' in element) {
+            return <InputCheckboxComponent key={index} inputCheckbox={element as InputCheckbox} />;
+          } else if ('select' in element) {
+            return <InputSelectComponent key={index} inputSelect={element as InputSelect} />;
+          }
+          return null;
+        })}
       </header>
     </div>
   );
